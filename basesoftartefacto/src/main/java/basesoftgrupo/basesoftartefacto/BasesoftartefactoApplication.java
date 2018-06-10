0b498d274071bf408c5846dc219006f0ceec05bf
fransao <francisco-salazar8a@hotmail.com>
2018-06-10 15:25:45 -0500

    Proyecto nuevo

---
+++ basesoftartefacto/src/main/java/basesoftgrupo/basesoftartefacto/BasesoftartefactoApplication.java
@@ -0,0 +1,12 @@
+package basesoftgrupo.basesoftartefacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasesoftartefactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasesoftartefactoApplication.class, args);
+	}
+}
